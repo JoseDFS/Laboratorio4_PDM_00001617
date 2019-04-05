0466a9356570c0f3534fc5a17380a0ab9eed3d82
Jose Segura <00001617@uca.edu.sv>
2019-04-05 10:47:59 -0600

    first commit

---
+++ app/src/main/java/com/example/labo4/activities/MovieActivity.kt
@@ -0,0 +1,13 @@
+package com.example.labo4.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.labo4.R
+
+class MovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
